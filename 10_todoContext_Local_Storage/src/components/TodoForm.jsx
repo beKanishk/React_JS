a6000befc3d8af4d8625e24ad1d144cb5c6a614b
Iamhacktree <akanishk76@gmail.com>
2025-07-23 23:06:02 +0530

    comments

--- 10_todoContext_Local_Storage/src/components/TodoForm.jsx
+++ 10_todoContext_Local_Storage/src/components/TodoForm.jsx
@@ -11,7 +11,7 @@ function TodoForm() {
       if (!todo) return
 
       addTodo({ todo : todo, completed: false})
-      setTodo("")//again we have chnaged input text value to null
+      setTodo("")//again we have changed input text value to null
     }
 
   return (
@@ -20,7 +20,7 @@ function TodoForm() {
               type="text"
               placeholder="Write Todo..."
               className="w-full border border-black/10 rounded-l-lg px-3 outline-none duration-150 bg-white/20 py-1.5"
-              value={todo}
+              value='{todo}'
               onChange={(e) => setTodo(e.target.value)}
           />
           <button type="submit" className="rounded-r-lg px-3 py-1 bg-green-600 text-white shrink-0">
